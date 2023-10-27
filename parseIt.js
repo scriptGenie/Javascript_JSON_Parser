b0442afe59e473d68cdca5c54f9ebc92f3c1d07e
scriptGenie <scriptGenie@protonmail.com>
2023-10-27 02:34:56 -0400

    Initial commit - package.json & parseIt.js

---
+++ parseIt.js
@@ -0,0 +1,202 @@
+const fs = require('fs');
+
+
+function parseIt() {
+
+    // // console.log(`0: ` + process.argv[0])
+    // // console.log(`1: ` + process.argv[1])
+    // // console.log(`2: ` + process.argv[2])
+    // // console.log(`3: ` + process.argv[3])
+    // // console.log(`4: ` + process.argv[4])
+
+
+    var fileNames = [];
+
+
+    // // console.log(`process.argv.length: ` + process.argv.length)
+
+
+    // if no filename provided
+    if (process.argv[2] == undefined) {
+        // console.log('Use -h for help.')
+        return 1;
+    };
+
+
+    const helpCommand = process.argv[2];
+
+    //help
+    if (['-h', '--help'].includes(helpCommand)) {
+        
+                    // ansi escape code - bold
+        console.log('\n\x1b[1mNAME\x1b[0m \n       Simple JSON Parser - \n       parseIt - return true if valid json object, else false')
+
+                    // ansi escape code - bold
+        console.log('\x1b[1mSYNOPSIS\x1b[0m');
+        console.log('       \x1b[1mparseIt\x1b[0m [FILE]... [File2ElectricBoogaloo]... [Fil3]...\n');
+
+        console.log('       -h, --help \n              you are here\n');
+        return 0;
+    };
+
+
+    // skip first two items in process.argv
+    for (let i = 2; i < process.argv.length; i++) {
+        fileNames.push(process.argv[i]);
+        // // console.log(process.argv[i]);
+    };
+
+
+    // meat and potatos here
+    if (fileNames.length > 0) {
+
+        for (let i = 0; i < fileNames.length; i++) {
+
+            var currentFile = fileNames[i];
+            const allFileContents = fs.readFileSync(currentFile, 'utf-8');
+            var linesInObject = allFileContents.split(/\r?\n/);
+            var cleanLines = [];
+
+            console.log('START - linesInObject length: ' + linesInObject.length)
+            console.log()
+
+            for (let p = 0; p < linesInObject.length; p++) {
+                //console.log(p)
+
+                let currentLine = linesInObject[p].trim();
+                //console.log(currentLine)
+
+                if (currentLine != '') {
+                    cleanLines.push(currentLine);
+                };
+            };
+
+
+            // weed out blank files
+            if (cleanLines.length == 0) {
+                console.log('BEEP BOOP BEEP - BLANK FILE DETECTED');
+                return 1;
+            };
+
+
+            // check for trailing comma in contents && object
+            if (cleanLines[cleanLines.length - 1].slice(-1) == ',' || cleanLines[cleanLines.length - 1].slice(-2, -1) == ',') {
+                console.log('Error: Trailing comma detected in object or contents');
+                return 1;
+            };
+
+
+            var singleLine = false
+
+            // check first and last char match curly braces
+            function checkCurlyBraces() {
+
+                console.log('BRACES - cleanLines length: ' + cleanLines.length)
+ 
+                var firstChar = cleanLines[0];
+                firstChar = firstChar[0];
+    
+                // var lastCharSingle = cleanLines[cleanLines.length - 1];
+                // lastCharSingle = lastCharSingle[lastCharSingle.length - 1];
+    
+                // var lastCharMulti = cleanLines[cleanLines.length - 1];
+                // lastCharMulti = lastCharMulti[lastCharMulti.length - 1];
+    
+                var lastChar = cleanLines[cleanLines.length - 1];
+                lastChar = lastChar[lastChar.length - 1];
+
+                if (cleanLines.length = 1) {    
+                    singleLine = true;
+                };
+
+
+                    // if single line check first and last char
+                    if (firstChar == '{' && lastChar == '}') {
+                        console.log('VALID JSON OBJECT - BRACES');
+                    } else {
+                        console.log('INVALID JSON OBJECT - BRACES');
+                        return false;
+                    };
+
+                // } else if (cleanLines.length > 1) {
+                    
+                //     singleLine = false;
+
+                //     // if more than one line in object check for first char on first line, and last char on last line
+                //     if (firstChar == '{' && lastCharMulti == '}') {
+                //         console.log('VALID JSON OBJECT - MULTI');
+                //     } else {
+                //         console.log('INVALID JSON OBJECT - MULTI');
+                //         return false;
+                //     };
+                    
+                // };
+            };
+
+
+            // check contents of object against base pattern
+            function checkPattern() {
+
+
+                // pattern - opening quote (") - must be start of string (^)
+                    // any alphanumeric char, any number of times, followed by an optional space 0 or 1 times
+                        // closing quote (")
+                            // colon (:)
+                                // space
+                                    // any alphanumeric char, any number of times, followed by an optional space 0 or 1 times
+                                        // closing quote (") - must be end of string ($)
+                var pattern = RegExp(/^"(\w* ?)*": "?(\w* ?)*"?/);
+                
+                console.log('ISSUE - cleanLines length: ' + cleanLines.length)
+                console.log(cleanLines)
+                console.log()
+
+                var resultsOfAllChecks = new Set([]);
+
+                for (let n = 1; n < cleanLines.length - 1; n++) {
+
+                    if (singleLine) {
+                        var contents = cleanLines[n].slice(1, -1);
+                    } else {
+                        var contents = cleanLines[n].slice(0, cleanLines[n].length);
+                    };
+
+
+                    console.log(n)
+                    console.log(contents)
+
+                    console.log(`pattern match:          ${pattern.test(contents)}`)
+                    console.log()
+
+                    if (pattern.test(contents)) {
+                        //console.log('worked')
+                        resultsOfAllChecks.add(true);
+                    } else {
+                        //console.log(`didn't worked`)
+                        resultsOfAllChecks.add(false);
+                    };
+
+                };
+                console.log(`resultsOfAllChecks:               ${Array.from(resultsOfAllChecks)}`)
+            };
+            
+
+
+            // if (checkPattern() && checkCurlyBraces()) {
+
+            // };
+
+
+            checkCurlyBraces()
+            checkPattern()
+
+        };
+
+    };
+
+};
+
+parseIt();
+
+
+
